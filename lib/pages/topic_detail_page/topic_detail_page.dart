f3ecb11c20de721dbeb9ccfb42d094ec1a03779e
dashu33 <83408280@qq.com>
2026-07-15 00:15:41 +0800

    feat: add goBack action to topic progress gestures
    Allow binding progress pill swipe/long-press menu to Navigator.maybePop.
    Also restore missing app_icons import used by scrub preview icons.

--- lib/pages/topic_detail_page/topic_detail_page.dart
+++ lib/pages/topic_detail_page/topic_detail_page.dart
@@ -1633,6 +1633,10 @@ class _TopicDetailPageState extends ConsumerState<TopicDetailPage>
             .enterSearchMode();
       case ProgressGestureAction.refresh:
         unawaited(_handleRefresh());
+      case ProgressGestureAction.goBack:
+        if (mounted) {
+          unawaited(Navigator.of(context).maybePop());
+        }
     }
   }
 
--- lib/pages/topic_detail_page/widgets/topic_progress_gestures.dart
+++ lib/pages/topic_detail_page/widgets/topic_progress_gestures.dart
@@ -4,6 +4,7 @@ import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:app_icons/app_icons.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
